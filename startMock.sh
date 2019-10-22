43ebd0592407e9f080898c106fde23354dfe1c82
Mallikarjuna Mudiam <mmudiam@Mallikarjunas-MacBook-Pro.local>
2019-10-21 23:30:18 -0400

    updates to some files

--- startMock.sh
+++ startMock.sh
@@ -2,11 +2,11 @@
 pwd
 mkdir mappings
 mkdir mocks
-find resource -name '*.txt' | xargs -i cp {} mappings
-find resource -name '*.json' | xargs -i cp {} mappings
-find resource -name '*.xml' | xargs -i cp {} mappings
-find stubs -name '*.txt' | xargs -i cp {} mocks
-find stubs -name '*.json' | xargs -i cp {} mocks
-find stubs -name '*.xml' | xargs -i cp {} mocks
+find resource -iname \*.txt -exec cp {} mappings \;
+find resource -iname \*.json -exec cp {} mappings \;
+find resource -iname \*.xml -exec cp {} mappings \;
+find stubs -iname \*.txt -exec cp {} mocks \;
+find stubs -iname \*.json -exec cp {} mocks \;
+find stubs -iname \*.xml -exec cp {} mocks \;
 /usr/local/bin/docker-compose up -d --force-recreate --build &&
 exit 0
